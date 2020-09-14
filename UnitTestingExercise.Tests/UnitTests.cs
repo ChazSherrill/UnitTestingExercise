b972fef312721ec1599ca2cb5994b35e64de4b0f
Chaz Sherrill <chazsherrill@icloud.com>
2020-09-14 12:06:01 -0500

    Unit test finished

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -13,54 +13,56 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
-            
+            var tI83 = new Calculator();
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+
+            var actual = tI83.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(15, 5, 10)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
-
+            var ti84 = new Calculator();
             //Act
-
+            var actual = ti84.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(2, 2, 4)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
-
+            var ti85 = new Calculator();
             //Act
-
+            var actual = ti85.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(28, 9, 3)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
-
+            var ti86 = new Calculator();
             //Act
-
+            var actual = ti86.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
     }
--- UnitTestingExercise/Program.cs
+++ UnitTestingExercise/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace UnitTestingExercise
 {
     class Program
     {
-        static void Main(string[] args)
+
+        
+static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+           
+
         }
     }
 }
